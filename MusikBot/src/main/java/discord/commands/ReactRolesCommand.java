a78271c4b29523866feda63c47f675ddd90fff84
odaia <odai.alfadel@goergue.com>
2022-02-11 01:12:31 +0100

    Reactrole command modifation

--- MusikBot/src/main/java/discord/commands/ReactRolesCommand.java
+++ MusikBot/src/main/java/discord/commands/ReactRolesCommand.java
@@ -14,10 +14,11 @@ public class ReactRolesCommand implements ServerCommand {
 
 	@Override
 	public void preformCommand(Member member, TextChannel channel, Message message) {
-		// args[0] args[1] args[2] args[3] args[4]
-		// args[0] args[1] args[2] :ok: @Rolle
-
-		String[] args = message.getContentDisplay().split("\\s+");
+		//args[0] args[1]   args[2]     args[3] args[4]
+ 		//!reactrole #channel 0223948232352  :ok:     @Rolle
+		
+		String[] args = message.getContentDisplay().split(" ");
+		
 		
 
 			List<TextChannel> channels = message.getMentionedChannels();
@@ -25,41 +26,33 @@ public class ReactRolesCommand implements ServerCommand {
 			List<Emote> emotes = message.getEmotes();
 			
 			
-			if (!channels.isEmpty() && !roles.isEmpty()) {
+			if(!channels.isEmpty() && !roles.isEmpty()) {
 				TextChannel tc = channels.get(0);
 				Role role = roles.get(0);
 				String messageIDString = args[2];
 
+				
 				try {
-					
 					long messageID = Long.parseLong(messageIDString);
 					
 					if(!emotes.isEmpty()) {
-						Emote emote = emotes.get(0);					
-
-					tc.addReactionById(messageID, emote).queue();
-					
-					LiteSQL.onUpdate(
-							"INSERT INTO reactroles(guildid, channelid, messageid, emote, rollenid) VALUES("
-							+ channel.getGuild().getIdLong() + "," + tc.getIdLong() + "," + message.getIdLong() + ",'"
-							+ emote + "'," + role.getIdLong() + ")");
-					}else {
+						Emote emote = emotes.get(0);
+						tc.addReactionById(messageID, emote).queue();
+						LiteSQL.onUpdate("INSERT INTO reactroles(guildid, channelid, messageid, emote, rollenid) VALUES(" +
+							channel.getGuild().getIdLong() + ", " + tc.getIdLong() + ", " + messageID + ", '" + emote.getId() + "', " + role.getIdLong() + ")");
+					}
+					else {
 						String emote = args[3];
 						tc.addReactionById(messageID, emote).queue();
-						LiteSQL.onUpdate("INSERT INTO reactroles(guildid, channelid, messageid, emote, rollenid) VALUES("
-								+ channel.getGuild().getIdLong() + "," + tc.getIdLong() + "," + message.getIdLong() + ",'"
-								+ emote + "'," + role.getIdLong() + ")");
+						LiteSQL.onUpdate("INSERT INTO reactroles(guildid, channelid, messageid, emote, rollenid) VALUES(" +
+								channel.getGuild().getIdLong() + ", " + tc.getIdLong() + ", " + messageID + ", '" + emote + "', " + role.getIdLong() + ")");
 					}
-					
-				} catch (NumberFormatException e) {
-
+				} catch (NumberFormatException e) { 
+					e.printStackTrace();
 				}
-			
-		} else {
-			channel.sendMessage("Bitte benutze `-reactrole #channel messageID :emote: @Rolle`").queue();
-			
-		}
-			message.delete().queue();
+			}
+			else {
+				channel.sendMessage("Bitte benutze `!reactrole #channel messageID :emote: @Rolle`").queue();
+			}
 	}
-
 }
